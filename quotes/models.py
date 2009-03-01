f700ba2e790a2092a13c8495ac36d1b25049b743
David Merwin <dave@davemerwin.com>
2009-03-01 11:55:26 -0800

    its cirq not circ

--- quotes/models.py
+++ quotes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Quote(models.Model):
     slug = models.SlugField(max_length=255, unique=True, help_text='A shortname for the quote.')
     author = models.CharField(blank=True, max_length=255, help_text='The quote\'s author')
-    circ = models.CharField(blank=True, max_length=100, help_text="When was the quote created?")
+    cirq = models.CharField(blank=True, max_length=100, help_text="When was the quote created?")
     quote = models.TextField()
 
     class Meta:
